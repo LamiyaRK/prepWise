392dc363951fe97294b27e92620bee8f0afcdcee
Lamiya <akabane755@gmail.com>
2026-07-25 20:44:14 +0600

    fixed issues

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -51,7 +51,7 @@ app.use('/api/companies', companyInsightsRouter)
 app.use('/api/resumes', resumeRouter)
 app.use('/api/streak', streakRouter)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
